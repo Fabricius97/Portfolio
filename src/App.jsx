a788b629830c3a09a413578e1b47f75aa08dd46e
Fabricius97 <christoffer.fabricius@hotmail.com>
2023-07-12 14:19:51 +0200

    Skills component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,79 @@
+import { Box, Grid } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3Alt,
+  faReact,
+  faSquareJs,
+  faNodeJs,
+  faGit,
+} from "@fortawesome/free-brands-svg-icons";
+import React from "react";
+
+const skillsList = [
+  { skillName: "React", icon: faReact },
+  { skillName: "Javascript", icon: faSquareJs },
+  { skillName: "CSS", icon: faCss3Alt },
+  { skillName: "HTML", icon: faHtml5 },
+  { skillName: "Node.js", icon: faNodeJs },
+  { skillName: "git", icon: faGit },
+  // Lägg till fler färdigheter här
+];
+
+const Skills = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "50vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: "100%",
+          padding: { xs: "0 30px", sm: "0 43px", md: "0 100px" },
+          height: "200px",
+        }}
+      >
+        <Grid container justifyContent="center" spacing={2}>
+          {skillsList.map((skill, index) => (
+            <Grid item key={index}>
+              <div
+                style={{
+                  width: 200,
+                  height: 200,
+                  borderRadius: "20px",
+                  backgroundColor: "#050a0d50",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  gap: "10px",
+                  transition:
+                    "opacity 0.5s ease-in-out, transform 0.3s ease-in-out",
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.opacity = "0.7";
+                  e.target.style.transform = "scale(1.1)";
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.opacity = "1";
+                  e.target.style.transform = "scale(1)";
+                }}
+              >
+                <FontAwesomeIcon icon={skill.icon} size="3x" />
+                <div>{skill.skillName}</div>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+export default Skills;
